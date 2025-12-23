553f476799f56ee52a8c5ca048f97f50c1da4a31
ai729776 <ai729776@gmail.com>
2025-12-23 16:40:16 +0000

    Remove ignoreBuildErrors from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  
-  // eslint অংশটি মুছে ফেলা হয়েছে কারণ এটি এরর দিচ্ছে
-  
-  // টাইপস্ক্রিপ্ট এরর এড়ানোর জন্য এটি থাকুক
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 };
 
 export default nextConfig;
